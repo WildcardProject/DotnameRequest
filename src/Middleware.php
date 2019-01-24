872f681923b522b5d0d20fbaedf3e3378d0f7c37
山崎 <yamazaki@assiette.co.jp>
2019-01-24 16:20:51 +0900

    "." を "_" に変換されてもちゃんと配列化できるようにしてみた。

--- src/Middleware.php
+++ src/Middleware.php
@@ -14,10 +14,25 @@ class Middleware
      */
     public function handle($request, Closure $next)
     {
-        $requestVars = [];
-        foreach ($request->all() as $key=>$value) {
-            array_set($requestVars, $key, $value);
+        return $next($this->_reset_request($request));
+    }
+
+    protected function _reset_request($request)
+    {
+        $posted = file_get_contents('php://input');
+        $got = array_get($_SERVER, 'QUERY_STRING');
+
+        $inputs = array_merge([], $this->_parse($got), $this->_parse($posted));
+        return $request->replace($inputs);
+    }
+
+    protected function _parse($query) {
+        $input = [];
+        if (!strlen($query)) return $input;
+        foreach (explode('&', $query) as $keyvalue) {
+            list($key, $value) = explode('=', $keyvalue);
+            array_set($input, $key, $value);
         }
-        return $next($request->replace($requestVars));
+        return $input;
     }
 }
