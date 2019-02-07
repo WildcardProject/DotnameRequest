d7bf8bd236a096dbfa7005262aa6f9a45a401de6
山崎 <yamazaki@assiette.co.jp>
2019-02-07 16:33:12 +0900

    multipartなフォームでは php://input が使えないことがわかりましたので、サポート対象を限定しました。。。

--- src/Middleware.php
+++ src/Middleware.php
@@ -22,8 +22,15 @@ class Middleware
         $posted = file_get_contents('php://input');
         $got = array_get($_SERVER, 'QUERY_STRING');
 
-        $inputs = array_merge([], $this->_parse($got), $this->_parse($posted));
-        return $request->replace($inputs);
+        if (strpos($request->header('CONTENT_TYPE'), 'application/x-www-form-urlencoded') !== FALSE || in_array(strtoupper($request->server('REQUEST_METHOD', 'GET')), ["GET", "DELETE"])) {
+            $inputs = array_merge([], $this->_parse($got), $this->_parse($posted));
+            return $request->replace($inputs);
+        }
+        else {
+            // multipartの場合はあきらめる
+            report(new \Exception("multipartの場合のパラメータ名変換はサポートしていません。"));
+            return $request;
+        }
     }
 
     protected function _parse($query) {
